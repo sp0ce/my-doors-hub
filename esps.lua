62c5c33821177cb2b607db989204b054949918a3
sp0ce <140521968+sp0ce@users.noreply.github.com>
2025-01-01 07:09:59 +0300

    Update esps.lua

--- esps.lua
+++ esps.lua
@@ -5,8 +5,8 @@ local Character = Player.Character or Player.CharacterAdded:Wait()
 
 local CurrentRooms = workspace:WaitForChild("CurrentRooms")
 
-function Test(Part: BasePart, Color: Color3)
-	if not Part:FindFirstChild("Highlight") then
+function AddHighlight(Part: BasePart, Color: Color3)
+	if Part and not Part:FindFirstChild("Highlight") then
 		local Highlight = Instance.new("Highlight", Part)
 		Highlight.FillColor = Color
 		Highlight.OutlineColor = Color
@@ -14,26 +14,25 @@ function Test(Part: BasePart, Color: Color3)
 end
 
 function RoomScan(Room: Model)
-	for _, v in pairs(Room:GetChildren()) do
-		if v.Name == "Door" then
-			Test(v:FindFirstChild("Door"), Color3.new(1, 1, 1))
-		elseif v.Name == "Assets" then
-			for _, v in pairs(v:GetDescendants()) do
-				if v.Name == "ChestBox" then
-					Test(v:FindFirstChild("Main"), Color3.new(1, 1, 1))
-				elseif v.Name == "ChestBoxLocked" then
-					Test(v:FindFirstChild("Main"), Color3.new(1, 1, 1))
-				elseif v.Name == "KeyObtain" then
-					local a = v:FindFirstChild("Hitbox")
-					if a then
-						Test(a:FindFirstChild("Key"), Color3.new(1, 1, 1))
+	pcall(function()
+		for _, v in pairs(Room:GetChildren()) do
+			if v.Name == "Door" then
+				Test(v:FindFirstChild("Door"), Color3.new(1, 1, 1))
+			elseif v.Name == "Assets" then
+				for _, v in pairs(v:GetDescendants()) do
+					if v.Name == "ChestBox" then
+						Test(v.Main, Color3.new(1, 1, 1))
+					elseif v.Name == "ChestBoxLocked" then
+						Test(v.Main, Color3.new(1, 1, 1))
+					elseif v.Name == "KeyObtain" then
+						Test(v.Hitbox, Color3.new(1, 1, 1))
 					end
 				end
+			elseif v.Name == "Sideroom" then
+				RoomScan(v)
 			end
-		elseif v.Name == "Sideroom" then
-			RoomScan(v)
 		end
-	end
+	end)
 end
 
 while task.wait(.5) do
