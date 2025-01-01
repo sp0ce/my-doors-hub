15e620146b6af2252ae46f2f20afcd0c4acc2eee
sp0ce <140521968+sp0ce@users.noreply.github.com>
2025-01-01 19:40:06 +0300

    Update notifications.lua

--- notifications.lua
+++ notifications.lua
@@ -6,30 +6,30 @@ local Player = Players.LocalPlayer
 local AchievementsHolder = Player.PlayerGui:WaitForChild("MainUI"):WaitForChild("AchievementsHolder")
 local AchievementTemplate = AchievementsHolder:WaitForChild("Achievement"):Clone()
 
-function EntityNotification(Icon: string, Title: string, Desc: string, Reason: string)
+function MakeNotification(Color: Color3, Icon: string, Title: string, Desc: string, Reason: string)
 	local Notification: Frame = AchievementTemplate:Clone()
 	Notification.Parent = AchievementsHolder
 	Notification.Visible = true
 	
 	Notification.Frame.Position = UDim2.fromScale(1.5, 0)
 	
-	Notification.Frame.UIStroke.Color = Color3.fromHSV(0, .8, 1)
+	Notification.Frame.UIStroke.Color = Color
 	
-	Notification.Frame.Glow.ImageColor3 = Color3.fromHSV(0, .8, 1)
+	Notification.Frame.Glow.ImageColor3 = Color
 	
 	Notification.Frame.TextLabel.Text = "ENTITY SPAWNED"
-	Notification.Frame.TextLabel.TextColor3 = Color3.fromHSV(0, .8, 1)
+	Notification.Frame.TextLabel.TextColor3 = Color
 	
 	Notification.Frame.ImageLabel.Image = Icon
 	
 	Notification.Frame.Details.Title.Text = Title
-	Notification.Frame.Details.Title.TextColor3 = Color3.fromHSV(0, .8, 1)
+	Notification.Frame.Details.Title.TextColor3 = Color
 	
 	Notification.Frame.Details.Desc.Text = Desc
-	Notification.Frame.Details.Desc.TextColor3 = Color3.fromHSV(0, .8, 1)
+	Notification.Frame.Details.Desc.TextColor3 = Color
 	
 	Notification.Frame.Details.Reason.Text = Reason
-	Notification.Frame.Details.Reason.TextColor3 = Color3.fromHSV(0, .8, 1)
+	Notification.Frame.Details.Reason.TextColor3 = Color
 	
 	TweenService:Create(Notification.Frame, TweenInfo.new(1, Enum.EasingStyle.Circular, Enum.EasingDirection.Out), {Position = UDim2.fromScale(0, 0)}):Play()
 	TweenService:Create(Notification.Frame.Glow, TweenInfo.new(1.5, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), {ImageTransparency = 1}):Play()
@@ -42,12 +42,12 @@ end
 
 workspace.ChildAdded:Connect(function(Child)
 	if Child.Name == "RushMoving" then
-		EntityNotification("", "Rush", "", "")
+		MakeNotification(Color3.fromHSV(0, .8, 1), "", "Rush", "", "")
 	elseif Child.Name == "AmbushMoving" then
-		EntityNotification("", "Ambush", "", "")
+		MakeNotification(Color3.fromHSV(0, .8, 1), "", "Ambush", "", "")
 	elseif Child.Name == "GlitchAmbush" then
-		EntityNotification("rbxassetid://126782836032454", "AR0xMBUSH", "", Child:GetAttribute("HallucinateTarget") .. " picked up Glitch Fragment")
+		MakeNotification(Color3.fromHSV(.8, .8, 1), "rbxassetid://126782836032454", "AR0xMBUSH", "", Child:GetAttribute("HallucinateTarget") .. " picked up Glitch Fragment")
 	elseif Child.Name == "GlitchRush" then
-		EntityNotification("rbxassetid://130697114081443", "RNIUSHCg==", "", Child:GetAttribute("HallucinateTarget") .. " picked up Glitch Fragment")
+		MakeNotification(Color3.fromHSV(.8, .8, 1), "rbxassetid://130697114081443", "RNIUSHCg==", "", Child:GetAttribute("HallucinateTarget") .. " picked up Glitch Fragment")
 	end
 end)
