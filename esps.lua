e74041a514543e3546ad22c22f870b659c05e567
sp0ce <140521968+sp0ce@users.noreply.github.com>
2025-01-02 04:20:02 +0300

    Update esps.lua

--- esps.lua
+++ esps.lua
@@ -9,16 +9,14 @@ local Root: BasePart = Character:WaitForChild("HumanoidRootPart")
 local CurrentRooms = workspace:WaitForChild("CurrentRooms")
 
 local GPSs = {}
-local HLs = {}
 local Clr = false
 
-function Esp(Active, Object1, Object2, Text, Color)
-	local GPS = Object1:FindFirstChild("ObjectGPS")
-	local HL = Object2:FindFirstChild("ObjectHL")
+function Esp(Active, Part, Text, Color)
+	local GPS = Part:FindFirstChild("ObjectGPS")
 	if Active then
 		if SH_ESPS_GPS_ENABLED then
 			if not GPS then
-				GPS = Instance.new("BillboardGui", Object1)
+				GPS = Instance.new("BillboardGui", Part)
 				GPS.AlwaysOnTop = true
 				GPS.Size = UDim2.fromOffset(200, 50)
 				GPS.Name = "ObjectGPS"
@@ -45,10 +43,6 @@ function Esp(Active, Object1, Object2, Text, Color)
 					DL.Text = math.round((Object1.Position - Root.Position).Magnitude) .. "s"
 				end)
 				
-				GPS:GetPropertyChangedSignal("Parent"):Once(function()
-					GPS:Destroy()
-				end)
-				
 				table.insert(GPSs, GPS)
 			end
 		else
@@ -56,30 +50,9 @@ function Esp(Active, Object1, Object2, Text, Color)
 				GPS:Destroy()
 			end
 		end
-		if SH_ESPS_HL_ENABLED then
-			if not HL then
-				HL = Instance.new("Highlight", Object2)
-				HL.FillColor = Color
-				HL.OutlineColor = Color
-				HL.Name = "ObjectHL"
-				
-				HL:GetPropertyChangedSignal("Parent"):Once(function()
-					HL:Destroy()
-				end)
-				
-				table.insert(HLs, HL)
-			end
-		else
-			if HL then
-				HL:Destroy()
-			end
-		end
 	else
 		if GPS then
 			GPS:Destroy()
-		end
-		if HL then
-			HL:Destroy()
 		end
 	end
 end
@@ -91,17 +64,17 @@ function RoomScan(Room: Model)
 		elseif v.Name == "Assets" then
 			for i, v in pairs(v:GetDescendants()) do
 				if v.Name == "KeyObtain" then
-					pcall(function() Esp(SH_ESP_KEY, v.Hitbox, v, "Key", Color3.new(1, 1, 1)) end)
+					pcall(function() Esp(SH_ESP_KEY, v.Hitbox, "Key", Color3.new(1, 1, 1)) end)
 				elseif v.Name == "LeverForGate" then
-					pcall(function() Esp(SH_ESP_LEVER, v.Main, v.Main, "Lever", Color3.new(1, 1, 1)) end)
+					pcall(function() Esp(SH_ESP_LEVER, v.Main, "Lever", Color3.new(1, 1, 1)) end)
 				elseif v.Name == "TimerLever" then
-					pcall(function() Esp(SH_ESP_LEVER, v.Hitbox, v, "Lever", Color3.new(1, 1, 1)) end)
+					pcall(function() Esp(SH_ESP_LEVER, v.Hitbox, "Lever", Color3.new(1, 1, 1)) end)
 				elseif v.Name == "Lighter" then
-					pcall(function() Esp(SH_ESP_LOOT, v.Handle, v, "Lighter", Color3.new(1, 1, 1)) end)
+					pcall(function() Esp(SH_ESP_LOOT, v.Handle, "Lighter", Color3.new(1, 1, 1)) end)
 				elseif v.Name == "Smoothie" then
-					pcall(function() Esp(SH_ESP_LOOT, v.Handle, v, "Smoothie", Color3.new(1, 1, 1)) end)
+					pcall(function() Esp(SH_ESP_LOOT, v.Handle, "Smoothie", Color3.new(1, 1, 1)) end)
 				elseif v.Name == "GlitchCube" then
-					pcall(function() Esp(SH_ESP_LOOT, v.MainPart, v, "Glitch Fragment", Color3.new(1, 1, 1)) end)
+					pcall(function() Esp(SH_ESP_LOOT, v.MainPart, "Glitch Fragment", Color3.new(1, 1, 1)) end)
 				end
 			end
 		elseif v.Name == "Sideroom" then
@@ -120,11 +93,6 @@ task.spawn(function()
 		elseif Clr == false then
 			Clr = true
 			for i, v in pairs(GPSs) do
-				if v then
-					v:Destroy()
-				end
-			end
-			for i, v in pairs(HLs) do
 				if v then
 					v:Destroy()
 				end
